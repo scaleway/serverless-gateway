18d02d4aa4921a03d866e4e13b89ceb3a5a2420f
Simon Shillaker <sshillaker@scaleway.com>
2023-05-30 18:07:25 +0200

    Add GUI container

--- gateway/scripts/startup.sh
+++ gateway/scripts/startup.sh
@@ -12,16 +12,19 @@ if [ ! -z "$IS_ADMIN_CONTAINER" ]; then
 
     echo "Starting Kong admin"
     kong start -v -c /kong-conf/kong-admin.conf
+elif [ ! -z "$IS_GUI_CONTAINER" ]; then
+    echo "Starting Kong GUI"
+    kong start -v -c /kong-conf/kong-gui.conf
 else
     echo "Starting Kong"
     # Reference: https://docs.docker.com/config/containers/multi-service_container/
     kong start -v -c /kong-conf/kong.conf &
-    
+
     if [ ! -z "$FORWARD_METRICS" ]; then
         echo "Starting Grafana Agent"
         /scripts/run-grafana-agent.sh
     fi
-    
+
     fg %1
 fi
 
