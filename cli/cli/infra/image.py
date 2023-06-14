1fd3bf0c939a86d81ca3f387adba129f75e17702
Andy Méry <amery@scaleway.com>
2023-06-14 17:33:38 +0200

    feat: get image tag from CLI version

--- cli/cli/infra/image.py
+++ cli/cli/infra/image.py
@@ -1,5 +1,11 @@
+from importlib.metadata import version
+
 IMAGE_REGISTRY = "docker.io"
 IMAGE_ORG = "scaleway"
 IMAGE_NAME = "serverless-gateway"
-IMAGE_VERSION = "0.2.0"
+
+# Single source of truth for the image version
+# is the version of the package
+IMAGE_VERSION = version("scw-gateway")
+
 IMAGE_TAG = f"{IMAGE_REGISTRY}/{IMAGE_ORG}/{IMAGE_NAME}:{IMAGE_VERSION}"
