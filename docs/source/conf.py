24ec4405804568eb41598e2cc78efc3b09040f65
Simon Shillaker <sshillaker@scaleway.com>
2023-07-03 17:28:37 +0200

    Python requirements for build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,30 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = "scw_gateway"
 author = "Scaleway Serverless Team"
 project_copyright = f"2023, {author}"
 release = "0.0.1"
 version = release
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     "sphinx_rtd_theme",
-    "myst_parser",  # Markdown support
+    "myst_parser",  # Markdown
     "sphinx.ext.autodoc",
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+exclude_patterns = ["_build"]
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
