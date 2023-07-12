d421d5d509023a827f7e77eedaeb4c06500ce3c9
Andy Méry <amery@scaleway.com>
2023-07-12 17:45:29 +0200

    feat: improve error display (#49)
    * feat: improve error display
    
    * docs: change docstring
    
    * refactor: rename verbose to debug to be consistent with cli
    
    * chore: make tidy
    
    * fix: failure in tests
    
    * feat: add -d short flag for debug mode

--- cli/cli/cli.py
+++ cli/cli/cli.py
@@ -6,14 +6,33 @@ from loguru import logger
 from cli.commands.consumer import consumer as consumer_commands
 from cli.commands.dev import dev as dev_commands
 from cli.commands.domain import domain as domain_commands
+from cli.commands.human.errors import display_exception
 from cli.commands.infra import infra as infra_commands
 from cli.commands.jwt import jwt as jwt_commands
 from cli.commands.route import route as route_commands
 
 
 @click.group()
-def cli():
-    """CLI for managing the Scaleway gateway.
+@click.option(
+    "--debug",
+    "-d",
+    is_flag=True,
+    help="Enables debug mode.",
+    default=False,
+    envvar="DEBUG",
+)
+@click.option(
+    "--log-level",
+    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+    default="INFO",
+    show_default=True,
+    help="Set the log level.",
+    envvar="LOG_LEVEL",
+)
+def cli(debug: bool, log_level: str) -> None:
+    """CLI for managing the Scaleway Gateway.
+    Documentation is available at:
+    https://serverless-gateway.readthedocs.io/en/latest/
 
     Commands are grouped into subgroups. Run
 
@@ -27,7 +46,9 @@ def cli():
     """
     # Set log level
     logger.remove()
-    logger.add(sys.stderr, level="INFO")
+    logger.add(sys.stderr, level=log_level, backtrace=False)
+    if debug:
+        logger.add(sys.stderr, level="DEBUG", backtrace=True, diagnose=True)
 
 
 cli.add_command(consumer_commands)
@@ -36,3 +57,17 @@ cli.add_command(domain_commands)
 cli.add_command(infra_commands)
 cli.add_command(jwt_commands)
 cli.add_command(route_commands)
+
+
+def main():
+    """Entrypoint for the CLI."""
+    try:
+        cli()  # pylint: disable=no-value-for-parameter
+    except Exception as err:  # pylint: disable=broad-except
+        # Print the exception in the console
+        logger.opt(exception=err).debug("An error occurred")
+        display_exception(err)
+
+
+if __name__ == "__main__":
+    main()
