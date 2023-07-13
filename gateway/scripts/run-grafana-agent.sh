5e92e88f16614831d8d70e12d54188e20df890f1
Simon Shillaker <sshillaker@scaleway.com>
2023-07-13 14:44:36 +0200

    Add retry on deploy

--- gateway/scripts/run-grafana-agent.sh
+++ gateway/scripts/run-grafana-agent.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# We sleep here as we need to give the gateway time to start
+sleep 20
+
 /bin/grafana-agent                      \
     --config.expand-env                 \
     --config.file=/etc/agent/agent.yaml
